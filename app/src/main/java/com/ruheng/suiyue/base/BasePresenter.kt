fd267c6dd0d34f60115cbc0dcc71becfdfe8a98a
LRH1993 <799386120@qq.com>
2018-02-28 15:54:46 +0800

    MVP基类

---
+++ app/src/main/java/com/ruheng/suiyue/base/BasePresenter.kt
@@ -0,0 +1,8 @@
+package com.ruheng.suiyue.base
+
+/**
+ * Created by lvruheng on 2018/2/28.
+ */
+interface BasePresenter {
+    fun start()
+}
---
+++ app/src/main/java/com/ruheng/suiyue/base/BaseView.kt
@@ -0,0 +1,8 @@
+package com.ruheng.suiyue.base
+
+/**
+ * Created by lvruheng on 2018/2/28.
+ */
+interface BaseView<T> {
+    fun setPresenter(presenter:T)
+}
